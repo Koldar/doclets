6ec460c4aa3f3c0932a7e0ba8d8510200cb9c7b9
Massimo Bono <massimobono1@gmail.com>
2019-01-30 19:39:30 +0100

    implemented latex formula

--- doclet-root/basic-taglets/src/main/java/com/massimobono/doclets/taglets/MathFormula.java
+++ doclet-root/basic-taglets/src/main/java/com/massimobono/doclets/taglets/MathFormula.java
@@ -1,12 +1,28 @@
 package com.massimobono.doclets.taglets;
 
 
-import com.massimobono.doclets.commons.taglets.TagletVisitorContext;
-import com.massimobono.doclets.commons.taglets.block.AbstractTextBlockTaglet;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import com.massimobono.doclets.commons.LatexInvoker;
+import com.massimobono.doclets.commons.TagletVisitorContext;
 import com.massimobono.doclets.commons.taglets.inline.AbstractTextInlineTaglet;
 import com.sun.source.doctree.TextTree;
 
 public class MathFormula extends AbstractTextInlineTaglet {
+
+	private static final Logger LOG = Logger.getLogger(MathFormula.class.getName());
+	
+	public MathFormula() {
+
+	}
 
 	@Override
 	public String getName() {
@@ -14,8 +30,56 @@ public class MathFormula extends AbstractTextInlineTaglet {
 	}
 
 	@Override
-	public String visitText(TextTree arg0, TagletVisitorContext<String> arg1) {
-		return "";
+	public String visitBegin(TextTree arg0, TagletVisitorContext<String> arg1) {
+		
+		//fetch the resource
+		var content = "";
+		try {
+			var is = this.getClass().getResourceAsStream("/latexFormulaTemplate.tex");
+			LOG.info(String.format("input stream is " + is));
+			var bis = new BufferedReader(new InputStreamReader(is));
+			var sb = new StringBuilder();
+			while (true) {
+				String line = bis.readLine();
+				if (line == null) {
+					break;
+				}
+				sb.append(line + "\n");
+			}
+			content = sb.toString();
+		} catch (IOException e1) {
+			throw new RuntimeException(e1);
+		}
+		
+		//replace __FORMULA__ with the text content
+		content = content.replace("__FORMULA__", arg0.getBody());
+		
+		var latexInvoker = new LatexInvoker(
+				Paths.get("pdflatex"),
+				Paths.get("convert"),
+				"png",
+				Paths.get(""),
+				false,
+				"",
+				"",
+				false
+				);
+
+		try {
+			var imageFile = latexInvoker.invoke(content);
+			return String.format("<img src=\"%s\"/>", imageFile.toAbsolutePath().toFile());
+		} catch (Exception e) {
+			//TODO add warning logging
+			LOG.warning("Cannot generate dot image for the following content:");
+			LOG.warning(arg0.getBody());
+			e.printStackTrace();
+			return "";
+		}
+
+	}
+	
+	public String visitEnd(TextTree arg0, TagletVisitorContext<String> arg1) {
+		return arg1.getBuilding();
 	}
 
 }
