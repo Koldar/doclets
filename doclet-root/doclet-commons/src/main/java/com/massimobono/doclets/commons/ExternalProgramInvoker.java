ef7a5474a5be772b24804c58ea7c65a97f8bbb7c
Massimo Bono <massimobono1@gmail.com>
2019-01-30 19:38:27 +0100

    new api for extenral program invoker

--- doclet-root/doclet-commons/src/main/java/com/massimobono/doclets/commons/ExternalProgramInvoker.java
+++ doclet-root/doclet-commons/src/main/java/com/massimobono/doclets/commons/ExternalProgramInvoker.java
@@ -7,11 +7,26 @@ import java.io.InputStreamReader;
 import java.lang.ProcessBuilder.Redirect;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ExternalProgramInvoker {
 	
 	public ExternalProgramInvoker() {
 		
+	}
+	
+	/**
+	 * like {@link #invoke(Path, String...)} but every strings in {@code cmds} which are balk will be ignored
+	 * @param cwd the working directory where you want to execute the command
+	 * @param cmds the strings representing the command. Each of them will be joined with " ". null strings or blak strings will be ignored 
+	 * @return the exit status of the program, always 0
+	 */
+	public int invoke(Path cwd, String... cmds) {
+		var cmd = String.join(" ", Arrays.stream(cmds)
+			.filter(s -> s != null)
+			.filter(s -> !s.isBlank())
+			.collect(Collectors.toList()));
+		return this.invoke(cmd, cwd);
 	}
 	
 	public int invoke(String command, Path cwd) {
